3aee3ee1440fdd4dcc58837284e5eb8844d476d8
easyau <easyag827@naver.com>
2024-09-13 23:23:07 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1, num2;
+	printf("Input two integers: ");
+	scanf_s("%d %d", &num1, &num2);
+	printf("%d & %d = %d \n", num1, num2, num1 & num2);
+	printf("%d | %d = %d \n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d \n", num1, num2, num1 ^ num2);
+}
